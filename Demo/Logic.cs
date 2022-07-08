bd2b74ce011e36a54da041ba6d1d82772285a687
Trikcode <ayesiganobert@gmail.com>
2022-07-08 19:20:11 +0300

    Add project files.

---
+++ Demo/Logic.cs
@@ -0,0 +1,14 @@
+﻿namespace DependencyInjection.Demo
+{
+    public class Logic
+    {
+        public int Value1 { get; private set; }
+
+        public int Value2 { get; private set; }
+    public Logic()
+        {
+            Value1 = Random.Shared.Next(0, 100);
+            Value2 = Random.Shared.Next(0, 100);
+        }
+    }
+}
